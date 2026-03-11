df28fd44d573cecc3365e80e738df8cd1bf4c889
ryan <example@exmaple.com>
2026-03-11 19:26:51 +0800

    [优化] 菜单调整

--- atsf_server/web/lib/constants/navigation.ts
+++ atsf_server/web/lib/constants/navigation.ts
@@ -1,51 +1,44 @@
-import type { NavigationItem } from '@/types/navigation';
+import type {NavigationItem} from '@/types/navigation';
 
 export const dashboardNavigation: NavigationItem[] = [
-  {
-    href: '/',
-    label: '总览',
-    icon: 'home',
-  },
-  {
-    href: '/node',
-    label: '节点',
-    icon: 'node',
-  },
-  {
-    href: '/config-version',
-    label: '发布',
-    icon: 'release',
-  },
-  {
-    href: '/managed-domain',
-    label: '网站',
-    icon: 'website',
-    children: [
-      {
-        href: '/managed-domain',
-        label: '域名',
-        icon: 'domain',
-      },
-      {
-        href: '/tls-certificate',
-        label: '证书',
-        icon: 'certificate',
-      },
-      {
+    {
+        href: '/',
+        label: '总览',
+        icon: 'home',
+    },
+    {
+        href: '/node',
+        label: '节点',
+        icon: 'node',
+    },
+    {
         href: '/proxy-route',
         label: '反向代理',
         icon: 'proxy',
-      },
-    ],
-  },
-  {
-    href: '/user',
-    label: '用户',
-    icon: 'user',
-  },
-  {
-    href: '/setting',
-    label: '设置',
-    icon: 'setting',
-  },
+    },
+    {
+        href: '/config-version',
+        label: '发布',
+        icon: 'release',
+    },
+    {
+        href: '/managed-domain',
+        label: '域名',
+        icon: 'domain',
+    },
+    {
+        href: '/tls-certificate',
+        label: '证书',
+        icon: 'certificate',
+    },
+    {
+        href: '/user',
+        label: '用户',
+        icon: 'user',
+    },
+    {
+        href: '/setting',
+        label: '设置',
+        icon: 'setting',
+    },
 ];
